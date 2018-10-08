4fd0949731fee7c9a138db73e27be22ba50aff15
Borisov Semen <semen.borisov@stoloto.ru>
2018-10-08 13:22:06 +0300

    pinch, remove prevDistance, now scale depends on initial distance between pointers

--- src/components/webcam/webcam.js
+++ src/components/webcam/webcam.js
@@ -1,263 +1,254 @@
-import throttle from "./throttle";
-
 const pointersGesture = {};
 
 // looking for id of the second pointer
 function getAnotherPointer(pointerId) {
-  let secondGesturePointerId;
+	let secondGesturePointerId;
 
-  Object.keys(pointersGesture).some(gesturePointerId => {
-    if (gesturePointerId !== pointerId) {
-      secondGesturePointerId = gesturePointerId;
-      return true;
-    }
+	Object.keys(pointersGesture).some(gesturePointerId => {
+		if (gesturePointerId !== pointerId) {
+			secondGesturePointerId = gesturePointerId;
+			return true;
+		}
 
-    return false;
-  });
+		return false;
+	});
 
-  return secondGesturePointerId;
+	return secondGesturePointerId;
 }
 
 const pinchGesture = {
-  prevDistance: null
+	beginDistance: null
 };
 
 const rotateGesture = {
-  beginAngle: 0
+	beginAngle: 0
 };
 
 class Webcam {
-  constructor(element) {
-    this.element = {
-      container: element.querySelector(".webcam-field"),
-      image: element.querySelector(".webcam-image"),
-      brightnessDisplay: element.querySelector(
-        ".webcam-info_type-bright .webcam-info__value"
-      ),
-      scaleDisplay: element.querySelector(
-        ".webcam-info_type-zoom .webcam-info__value"
-      )
-    };
+	constructor(element) {
+		this.element = {
+			container: element.querySelector(".webcam-field"),
+			image: element.querySelector(".webcam-image"),
+			brightnessDisplay: element.querySelector(
+				".webcam-info_type-bright .webcam-info__value"
+			),
+			scaleDisplay: element.querySelector(".webcam-info_type-zoom .webcam-info__value")
+		};
 
-    this.image = {
-      height: parseInt(this.element.image.clientHeight)
-    };
+		this.image = {
+			height: parseInt(this.element.image.clientHeight)
+		};
 
-    this.brightness = {
-      MAX: 100,
-      MIN: 0
-    };
+		this.brightness = {
+			MAX: 100,
+			MIN: 0
+		};
 
-    this.scale = {
-      MAX: 1.5,
-      MIN: 0.25
-    };
+		this.scale = {
+			MAX: 1.5,
+			MIN: 0.25
+		};
 
-    this.settings = {
-      dx: 0,
-      scale: 1,
-      brightness: 50
-    };
+		this.settings = {
+			dx: 0,
+			scale: 1,
+			brightness: 50
+		};
 
-    this.initEvents();
+		this.initEvents();
+	}
 
-    this.tryPinchGesture = throttle(this.tryPinchGesture, 50);
-  }
+	renderIndicator(type, value) {
+		if (type === "brightness") {
+			this.element.brightnessDisplay.innerText = `${value}%`;
 
-  renderIndicator(type, value) {
-    if (type === "brightness") {
-      this.element.brightnessDisplay.innerText = `${value}%`;
+			this.element.container.style.filter = `brightness(${value / 100})`;
+		}
 
-      this.element.container.style.filter = `brightness(${value / 100})`;
-    }
+		if (type === "scale") {
+			this.element.scaleDisplay.innerText = `${(value * 100).toFixed(0)}%`;
 
-    if (type === "scale") {
-      this.element.scaleDisplay.innerText = `${(value * 100).toFixed(0)}%`;
+			this.element.image.style.transform = `scale(${value})`;
+		}
+	}
 
-      this.element.image.style.transform = `scale(${value})`;
-    }
-  }
+	changeBrightness(turnInRadians) {
+		// turn on 360 radians - means change brightness from zero to 100
+		const mapRadiansToBrightness = radians => {
+			const brightness = Math.ceil(100 / radians);
 
-  changeBrightness(turnInRadians) {
-    // turn on 360 radians - means change brightness from zero to 100
-    const mapRadiansToBrightness = radians => {
-      const brightness = Math.ceil(100 / radians);
+			return brightness;
+		};
 
-      return brightness;
-    };
+		const brightnessIncrement = mapRadiansToBrightness(turnInRadians);
 
-    const brightnessIncrement = mapRadiansToBrightness(turnInRadians);
+		// TODO: refactoring, [min, max]
+		if (this.settings.brightness + brightnessIncrement > this.brightness.MAX) {
+			this.settings.brightness = this.brightness.MAX;
+		} else if (this.settings.brightness + brightnessIncrement < this.brightness.MIN) {
+			this.settings.brightness = this.brightness.MIN;
+		} else {
+			this.settings.brightness += brightnessIncrement;
+		}
 
-    // TODO: refactoring, [min, max]
-    if (this.settings.brightness + brightnessIncrement > this.brightness.MAX) {
-      this.settings.brightness = this.brightness.MAX;
-    } else if (
-      this.settings.brightness + brightnessIncrement <
-      this.brightness.MIN
-    ) {
-      this.settings.brightness = this.brightness.MIN;
-    } else {
-      this.settings.brightness += brightnessIncrement;
-    }
+		this.renderIndicator("brightness", this.settings.brightness);
+	}
 
-    this.renderIndicator("brightness", this.settings.brightness);
-  }
+	scaleCamera(dy) {
+		// нужно понять, как превратить dy в scale изображения
+		// сейчас проблема в том, что я прибавляю scaleRatio к предыдущему значению
 
-  scaleCamera(dy) {
-    const scaleRatio = dy * 0.001;
+		const scaleRatio = dy * 0.001;
 
-    // TODO: refactoring, [min, max]
-    if (this.settings.scale + scaleRatio > this.scale.MAX) {
-      this.settings.scale = this.scale.MAX;
-    } else if (this.settings.scale + scaleRatio < this.scale.MIN) {
-      this.settings.scale = this.scale.MIN;
-    } else {
-      this.settings.scale += scaleRatio;
-    }
+		// TODO: refactoring, [min, max]
+		if (this.settings.scale + scaleRatio > this.scale.MAX) {
+			this.settings.scale = this.scale.MAX;
+		} else if (this.settings.scale + scaleRatio < this.scale.MIN) {
+			this.settings.scale = this.scale.MIN;
+		} else {
+			this.settings.scale += scaleRatio;
+		}
 
-    // console.log("scale", scaleRatio, `scale(${this.settings.scale})`);
+		// console.log("scale", scaleRatio, `scale(${this.settings.scale})`);
 
-    this.renderIndicator("scale", this.settings.scale);
-  }
+		this.renderIndicator("scale", this.settings.scale);
+	}
 
-  moveCamera(dx) {
-    this.settings.dx += dx;
+	moveCamera(dx) {
+		this.settings.dx += dx;
 
-    this.element.image.style.backgroundPosition = this.settings.dx + "px";
-  }
+		this.element.image.style.backgroundPosition = this.settings.dx + "px";
+	}
 
-  initEvents() {
-    this.element.container.addEventListener("pointerdown", this.addPointer);
+	initEvents() {
+		this.element.container.addEventListener("pointerdown", this.addPointer);
 
-    ["pointerup", "pointerout", "pointerleave", "pointercancel"].forEach(
-      event => {
-        this.element.container.addEventListener(event, this.removePointer);
-      }
-    );
+		["pointerup", "pointerout", "pointerleave", "pointercancel"].forEach(event => {
+			this.element.container.addEventListener(event, this.removePointer);
+		});
 
-    this.element.container.addEventListener(
-      "pointermove",
-      this.movePointer.bind(this)
-    );
-  }
+		this.element.container.addEventListener("pointermove", this.movePointer.bind(this));
+	}
 
-  addPointer(e) {
-    pointersGesture[e.pointerId] = {
-      prevX: e.x,
-      prevY: e.y,
-      startX: e.x,
-      startY: e.y,
-      isPrimary: e.isPrimary,
-      id: e.pointerId
-    };
-  }
+	addPointer(e) {
+		pointersGesture[e.pointerId] = {
+			prevX: e.x,
+			prevY: e.y,
+			startX: e.x,
+			startY: e.y,
+			isPrimary: e.isPrimary,
+			id: e.pointerId
+		};
+	}
 
-  removePointer(e) {
-    delete pointersGesture[e.pointerId];
+	removePointer(e) {
+		delete pointersGesture[e.pointerId];
 
-    pinchGesture.prevDistance = null;
-    rotateGesture.beginAngle = null;
-  }
+		pinchGesture.beginDistance = null;
+		rotateGesture.beginAngle = null;
+	}
 
-  movePointer(e) {
-    const { pointerId, x, y } = e;
-    const gesture = pointersGesture[pointerId];
+	movePointer(e) {
+		const { pointerId, x, y } = e;
+		const gesture = pointersGesture[pointerId];
 
-    if (!gesture) {
-      return null;
-    }
+		if (!gesture) {
+			return null;
+		}
 
-    const isOnePointerMove = Object.keys(pointersGesture).length === 1;
-    const isPartOfTwoPointersMove = Object.keys(pointersGesture).length === 2;
+		const isOnePointerMove = Object.keys(pointersGesture).length === 1;
+		const isPartOfTwoPointersMove = Object.keys(pointersGesture).length === 2;
 
-    // one pointer means camera move gesture
-    if (isOnePointerMove) {
-      const dx = x - gesture.prevX;
+		// one pointer means camera move gesture
+		if (isOnePointerMove) {
+			const dx = x - gesture.prevX;
 
-      this.moveCamera(dx);
-    }
+			this.moveCamera(dx);
+		}
 
-    // two pointers can be rotate or pinch gesture
-    if (isPartOfTwoPointersMove) {
-      let secondGesturePointerId = getAnotherPointer(pointerId);
+		// two pointers can be rotate or pinch gesture
+		if (isPartOfTwoPointersMove) {
+			let secondGesturePointerId = getAnotherPointer(pointerId);
 
-      // TODO: check, that rotate isn't active now
-      // rotate gesture is primary, it's  a corner case of pinch
+			// TODO: check, that rotate isn't active now
+			// rotate gesture is primary, it's  a corner case of pinch
 
-      this.tryPinchGesture({
-        event: event,
-        secondGesture: pointersGesture[secondGesturePointerId]
-      });
+			this.tryPinchGesture({
+				event: event,
+				firstGesture: pointersGesture[pointerId],
+				secondGesture: pointersGesture[secondGesturePointerId]
+			});
 
-      // rotate gesture
-      this.tryRotateGesture({
-        event: event,
-        firstGesture: pointersGesture[pointerId],
-        secondGesture: pointersGesture[secondGesturePointerId]
-      });
-    }
+			// rotate gesture
+			this.tryRotateGesture({
+				event: event,
+				firstGesture: pointersGesture[pointerId],
+				secondGesture: pointersGesture[secondGesturePointerId]
+			});
+		}
 
-    pointersGesture[pointerId].prevX = x;
-    pointersGesture[pointerId].prevY = y;
-  }
+		pointersGesture[pointerId].prevX = x;
+		pointersGesture[pointerId].prevY = y;
+	}
 
-  tryPinchGesture({ event, secondGesture }) {
-    const currentDistance = Math.hypot(
-      event.x - secondGesture.prevX,
-      event.y - secondGesture.prevY
-    );
+	tryPinchGesture({ event, firstGesture, secondGesture }) {
+		if (!pinchGesture.beginDistance) {
+			const beginDistance = Math.hypot(
+				firstGesture.startX - secondGesture.startX,
+				firstGesture.startY - secondGesture.startY
+			);
 
-    // TODO: think about beginDistance here
-    // now I calculate DY as function from previous pointer moving distance
-    // it creates trouble with shaking image
+			pinchGesture.beginDistance = beginDistance;
+		}
 
-    if (pinchGesture.prevDistance > 0) {
-      const dy = currentDistance - pinchGesture.prevDistance;
+		const currentDistance = Math.hypot(
+			event.x - secondGesture.prevX,
+			event.y - secondGesture.prevY
+		);
 
-      // pinch down
-      this.scaleCamera(dy);
-    }
+		const dy = currentDistance - pinchGesture.beginDistance;
 
-    pinchGesture.prevDistance = currentDistance;
-  }
+		// pinch down
+		this.scaleCamera(dy);
+	}
 
-  tryRotateGesture({ event, firstGesture, secondGesture }) {
-    // TODO: check distance between pointers,
-    // in rotate gesture, distance between pointers should be consistent
+	tryRotateGesture({ event, firstGesture, secondGesture }) {
+		// TODO: check distance between pointers,
+		// in rotate gesture, distance between pointers should be consistent
 
-    // caclulate beginAngle, angle in moment, when pointers was inited
-    if (!pinchGesture.beginAngle) {
-      const x1 = firstGesture.startX;
-      const y1 = firstGesture.startY;
+		// caclulate beginAngle, angle in moment, when pointers was inited
+		if (!rotateGesture.beginAngle) {
+			const x1 = firstGesture.startX;
+			const y1 = firstGesture.startY;
 
-      const x2 = secondGesture.startX;
-      const y2 = secondGesture.startY;
+			const x2 = secondGesture.startX;
+			const y2 = secondGesture.startY;
 
-      const dx = x2 - x1;
-      const dy = y2 - y1;
+			const dx = x2 - x1;
+			const dy = y2 - y1;
 
-      const beginAngle = Math.atan2(dy, dx);
+			const beginAngle = Math.atan2(dy, dx);
 
-      pinchGesture.beginAngle = beginAngle;
-    }
+			rotateGesture.beginAngle = beginAngle;
+		}
 
-    const x1 = event.x;
-    const y1 = event.y;
+		const x1 = event.x;
+		const y1 = event.y;
 
-    const x2 = secondGesture.prevX;
-    const y2 = secondGesture.prevY;
+		const x2 = secondGesture.prevX;
+		const y2 = secondGesture.prevY;
 
-    // calculate CURRENT angle between fingers
-    const dx = x2 - x1;
-    const dy = y2 - y1;
+		// calculate CURRENT angle between fingers
+		const dx = x2 - x1;
+		const dy = y2 - y1;
 
-    const currentAngle = Math.atan2(dy, dx);
-    const changeAngle = currentAngle - pinchGesture.beginAngle;
+		const currentAngle = Math.atan2(dy, dx);
+		const changeAngle = currentAngle - rotateGesture.beginAngle;
 
-    const rotationAngle = (changeAngle * 180) / Math.PI;
+		const rotationAngle = (changeAngle * 180) / Math.PI;
 
-    this.changeBrightness(rotationAngle);
-  }
+		this.changeBrightness(rotationAngle);
+	}
 }
 
 export default Webcam;
